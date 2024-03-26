86bf469d925926e47b1904aa03d0eb5b0c8f21b8
Akira Nomura <anomura824@gmail.com>
2024-03-26 14:18:34 +0000

    build: streamlit page framework

--- app/app.py
+++ app/app.py
@@ -1,3 +1,192 @@
 import streamlit as st
 
-st.header('Hello, world')
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from joblib import load
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scikitplot as skplt
+
+from lime import lime_tabular
+
+
+#import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import time
+
+import plotly.express as px
+
+from utils import make_sidebar, cover_page, load_dataset, select_ml_algorithm
+
+warnings.filterwarnings('ignore')
+
+st.set_page_config(
+    # page_title="Ex-stream-ly Cool App",
+    # page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+make_sidebar()
+cover_page()
+
+df, target_class_name = load_dataset(st.session_state['dataset'])
+
+st.header('Data', divider='orange')
+
+tab1, tab2 = st.tabs(['Column Summary','Data Table'])
+
+with tab1:
+    for col in df.columns[:-1]:
+        with st.container():
+            with st.expander(f'{col}'):
+            # st.markdown(f'''<b>{col} </b>''', unsafe_allow_html=True)
+                col1, col2, col3, col4 = st.columns([1,4,3,3])
+                with col1:
+                    st.write('')
+                with col2:
+                    if st.checkbox('Show target class',key=f'{col}'):
+                        color = 'target'
+                    else:
+                        color = None
+
+                    fig = px.histogram(df, x= col, color=color,width=500, height=300)
+                    st.plotly_chart(fig)
+                with col3:
+                    st.write('')
+                with col4:
+                    stats = df[col].describe()
+                    stats.loc['missing'] = df[col].isnull().sum()
+                    st.write(stats)
+
+with tab2:
+    st.dataframe(df, use_container_width=True, hide_index=True)
+
+st.header('Data preprocessing',divider='orange')
+
+with st.expander('Missing Data Handling'):
+    st.write(df.isnull().sum())
+
+with st.expander('Data Normalization'):
+    st.write('----------')
+
+st.header('Data Inspection', divider='orange')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader('Scatterplot Matrix')
+    st.write('Visualization of the relationship between each pair of variables')
+    pair_fig =  sns.pairplot(df, hue='target',
+                            #  palette='husl',
+                            markers=['o','s','D'],
+                            corner=True)
+    st.pyplot(pair_fig)
+
+with col2:
+    st.subheader('Correlation plot')
+    st.write('compute the coreraltion coefficient')
+    corr = df.drop('target',axis=1).corr()
+    corr_fig, ax = plt.subplots(figsize=(5,4))
+    sns.heatmap(corr,
+                annot=True,
+                ax=ax,
+                )
+    st.pyplot(corr_fig)
+
+st.header('Model Training', divider='orange')
+
+X = df.drop(columns=['target'])
+Y = df['target']
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=st.session_state['test_size'])
+
+st.subheader('Data structure')
+with st.container():
+    col1, col2, col3 =st.columns([0.5,4,1])
+    with col1:
+        st.write('')
+    with col2:
+        st.code(f"""
+                    Shape of Predictor dataset  :   {X.shape} \n
+                    Shape of Trainng dataset    :   {x_train.shape} \n
+                    Shape of Testing dataset    :   {x_test.shape} \n
+                    Number of Class             :   {len(np.unique(Y))} \n
+                    Target Class                :   {target_class_name} \n
+                """)
+    with col3:
+        st.write('')
+
+st.subheader('Model construction')
+start_time = time.time()
+model = select_ml_algorithm(st.session_state['algorithm'], st.session_state['params'])
+model.fit(x_train, y_train)
+elapsed_time = time.time() - start_time
+
+if model:
+    col1, col2, col3 = st.columns([0.5,4,1])
+    with col1:
+        st.write('')
+    with col2:
+        with st.container():
+            st.code(f'''
+                    Model algorithm    : {st.session_state['algorithm']} \n
+                    Model parameters   : {st.session_state['params']}
+                    ''')
+            st.success('Model created successfully!', icon="✅")
+            st.write('')
+            st.code(f'Elapsed time for Model Training: {elapsed_time:.5f} seconds')
+    with col3:
+        st.write('')
+
+st.header('Model Prediction', divider='orange')
+st.divider()
+
+y_test_preds = model.predict(x_test)
+
+
+st.header("Confusion Matrix | Feature Importances", divider='orange')
+col1, col2, col3 = st.columns([1.5,0.5,2])
+with col1:
+    st.subheader('Confusion Matrix')
+    st.write('')
+    conf_mat_fig = plt.figure(figsize=(10,10))
+    ax1 = conf_mat_fig.add_subplot(111)
+    skplt.metrics.plot_confusion_matrix(y_test, y_test_preds, ax=ax1, normalize=True)
+    st.pyplot(conf_mat_fig)
+
+with col2:
+    st.write('')
+
+with col3:
+    if st.session_state['algorithm']=='GBC':
+        st.subheader('Feature Importance')
+        st.write('')
+        coefficients = model.coef_
+
+        avg_importance = np.mean(np.abs(coefficients), axis=0)
+        feature_importance = pd.DataFrame({'Feature': X.columns, 'Importance': avg_importance})
+        feature_importance = feature_importance.sort_values('Importance', ascending=True)
+
+        feature_importance_plot, ax = plt.subplots(figsize=(6, 4))
+        ax.barh(feature_importance['Feature'], feature_importance['Importance'])
+        ax.set_xlabel('Importance')
+        # ax.set_ylabel('Feature')
+        ax.set_title('Feature Importance')
+
+        st.pyplot(feature_importance_plot)
+
+
+st.header('Model Evaluation', divider='orange')
+st.subheader("Classification Report")
+st.code(classification_report(y_test, y_test_preds))
---
+++ app/utils.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import streamlit as st
+
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression,LogisticRegressionCV
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.model_selection import train_test_split,cross_val_score,cross_val_predict,ShuffleSplit,GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import scale
+from sklearn.metrics import roc_auc_score,roc_curve
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix,accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+# from xgboost import XGBClassifier
+
+@st.cache_data
+def load_dataset(selected_dataset):
+    if selected_dataset=="Iris":
+        data = datasets.load_iris()
+    elif selected_dataset=="Breast Cancer":
+        data = datasets.load_breast_cancer()
+    elif selected_dataset=="Wine Dataset":
+        data = datasets.load_wine()
+
+    feature_data = data.data
+    columns = data.feature_names
+    target = data.target
+    target_class_name = data.target_names
+
+    df = pd.DataFrame(feature_data, columns = columns)
+    df.loc[:,'target'] = target.reshape(-1,1)
+
+    return df, target_class_name
+
+def select_ml_algorithm(algorithm,params):
+    if algorithm=='KNN':
+        alg = KNeighborsClassifier(n_neighbors=params['K'])
+    elif algorithm=='Logistic Regression':
+        alg = LogisticRegression(penalty=params['penalty'])
+    elif algorithm=='GBC':
+        alg = GradientBoostingClassifier(learning_rate=params['learning_rate'], n_estimators=params['n_estimators'])
+    elif algorithm=='Random Forests':
+        alg = RandomForestClassifier(n_estimators=params['n_estimators'])
+    elif algorithm=='Decision Tree':
+        alg = DecisionTreeClassifier(max_depth=params['max_depth'],criterion=params['criterion'])
+    elif algorithm=='Support Vector Machines':
+        alg = SVC(C=params['C'],kernel=params['kernel'])
+    return alg
+
+def optimize_hyperparameters(clf_name):
+    params = dict()
+    if clf_name=='KNN':
+        K = st.slider('K',1,15)
+        params['K']=K
+    elif clf_name=='Logistic Regression':
+        penalty = st.selectbox('penalty',('l2','none'))
+        params['penalty']=penalty
+    elif clf_name=='GBC':
+        learning_rate = st.slider('learning_rate',min_value=0.01,max_value=10.0, value=0.1)
+        n_estimators = st.slider('n_estimators',min_value=1,max_value=500, value=100)
+        params['learning_rate']=learning_rate
+        params['n_estimators']=n_estimators
+    elif clf_name=='Random Forests':
+        n_estimators = st.slider('n_estimators',min_value=0,max_value=1500,value=100)
+        params['n_estimators']=n_estimators
+    elif clf_name=='Decision Tree':
+        criterion=st.selectbox('criterion',('gini','entropy'))
+        max_depth = st.slider('max_depth',min_value=1,max_value=15)
+        params['criterion']=criterion
+        params['max_depth']=max_depth
+    elif clf_name=='Support Vector Machines':
+        C = st.slider('C',min_value=1,max_value=15)
+        kernel=st.selectbox('kernel',('linear', 'poly', 'rbf', 'sigmoid', 'precomputed'))
+        params['C']=C
+        params['kernel']=kernel
+
+    return params
+
+def make_sidebar():
+    with st.sidebar:
+        st.title('💎 Menu')
+        st.write('')
+        st.write('')
+
+        tab1, tab2, tab3 = st.tabs(['Data :clipboard:', 'Model parameters', 'Help'])
+
+        with tab1:
+
+            selected_dataset = st.selectbox('Select Dataset',('Iris','Breast Cancer','Wine Dataset'))
+            # selected_dataset_= 'Iris'
+            #Check out all available dataset: https://scikit-learn.org/stable/datasets/toy_dataset.html
+
+            st.subheader(f'{selected_dataset} dataset selected')
+
+            st.session_state['dataset'] = selected_dataset
+
+            selected_algorithm = st.selectbox('Select Algorithm',('Logistic Regression','Random Forests','GBC','Decision Tree','KNN','Support Vector Machines'))
+            # selected_algorithm = 'Logistic Regression'
+
+            st.subheader(f'{selected_algorithm} algorithm selected')
+
+            st.session_state['algorithm'] = selected_algorithm
+
+        with tab2:
+
+            st.write('')
+            st.subheader('Data Split')
+            with st.container(border=True):
+                st.session_state['test_size'] = st.slider(label='Test Data Size', min_value=0.01, max_value=0.99, value=0.3, step=0.1)
+                st.markdown(f'''<b>Training data </b>: {1- st.session_state['test_size']}</style>''', unsafe_allow_html=True)
+                st.markdown(f'''<b>Testing data </b>: {st.session_state['test_size']}''', unsafe_allow_html=True)
+            st.divider()
+            st.subheader('Tune model parameters')
+            st.session_state['params'] = optimize_hyperparameters(st.session_state['algorithm'])
+
+        with tab3:
+
+            st.write('short description of each dataset')
+
+@st.cache_data
+def load_dataset_description(selected_dataset):
+    if selected_dataset=="Iris":
+        data = datasets.load_iris()
+    elif selected_dataset=="Breast Cancer":
+        data = datasets.load_breast_cancer()
+    elif selected_dataset=="Wine Dataset":
+        data = datasets.load_wine()
+
+
+def cover_page():
+
+    st.header('Iris Classification')
+
+    col1,col2,col3 = st.columns([1,4,1])
+
+    with col1:
+        st.write('')
+    with col2:
+        st.image('https://miro.medium.com/v2/resize:fit:1400/format:webp/0*Uw37vrrKzeEWahdB')
+    with col3:
+        st.write('')
