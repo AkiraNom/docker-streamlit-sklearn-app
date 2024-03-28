1ea320eeccb7699cee58a8ebfea59cec1a96b7f2
Akira Nomura <anomura824@gmail.com>
2024-03-28 06:26:02 +0000

    add: selectbox for target class

--- app/app.py
+++ app/app.py
@@ -34,7 +34,8 @@ warnings.filterwarnings('ignore')
 st.set_page_config(
     # page_icon="🧊",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
+    # initial_sidebar_state="expanded",
 )
 
 make_sidebar()
@@ -43,46 +44,49 @@ cover_page()
 df, target_class_name = load_dataset(st.session_state['dataset'])
 
 st.header('Data', divider='orange')
+if 'target' not in st.session_state:
+    if 'target' in df.columns.tolist():
+        st.session_state['target'] = 'target'
+    else:
+        st.session_state['target'] = df.columns.tolist()[-1]
 
+col1, col2, col3 = st.columns([0.2,0.5,2])
+with col1:
+    st.write('')
+with col2:
+    if st.checkbox('Specify target class'):
+        st.write(st.session_state['target'])
+        default_ix = df.columns.tolist().index(st.session_state['target'])
+        selected_target = st.selectbox('Select target feature', df.columns.tolist(), index=default_ix)
+        st.session_state['target'] = selected_target
+with col3:
+    st.info('If the target class is neither in the target column nor at the last column')
 tab1, tab2 = st.tabs(['Data Table','Column Summary'])
 
 with tab1:
     st.dataframe(df, use_container_width=True, hide_index=True)
 
 with tab2:
-    for col in df.columns[:-1]:
+    for col_name in df.columns[:-1]:
         with st.container():
-            with st.expander(f'{col}'):
+            with st.expander(col_name):
                 col1, col2, col3, col4 = st.columns([1,4,3,3])
                 with col1:
                     st.write('')
                 with col2:
-                    if st.checkbox('Show target class',key=f'{col}'):
-                        color = 'target'
+                    if st.checkbox('Show target class',key=col_name):
+                        color = st.session_state['target']
                     else:
                         color = None
 
-                    fig = px.histogram(df, x= col, color=color,width=500, height=300)
+                    fig = px.histogram(df, x= col_name, color=color,width=500, height=300)
                     st.plotly_chart(fig)
                 with col3:
                     st.write('')
                 with col4:
-                    stats = df[col].describe()
-                    stats.loc['missing'] = df[col].isnull().sum()
+                    stats = df[col_name].describe()
+                    stats.loc['missing'] = df[col_name].isnull().sum()
                     st.write(stats)
-
-
-st.header('Data preprocessing',divider='orange')
-
-with st.expander('Missing Data Handling'):
-    col1, col2 = st.columns([0.5,4])
-    with col1:
-        st.write('')
-    with col2:
-        st.write(df.isnull().sum())
-
-with st.expander('Data Normalization'):
-    st.write('----------')
 
 st.header('Data Inspection', divider='orange')
 
@@ -90,7 +94,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader('Scatterplot Matrix')
     st.write('Visualization of the relationship between each pair of variables')
-    pair_fig =  sns.pairplot(df, hue='target',
+    pair_fig =  sns.pairplot(df, hue=st.session_state['target'],
                              markers=['o','s','D'],
                              corner=True)
     st.pyplot(pair_fig)
@@ -98,7 +102,7 @@ with col1:
 with col2:
     st.subheader('Correlation plot')
     st.write('compute the coreraltion coefficient')
-    corr = df.drop('target',axis=1).corr()
+    corr = df.drop(st.session_state['target'],axis=1).corr()
     corr_fig, ax = plt.subplots(figsize=(5,4))
     sns.heatmap(corr,
                 annot=True,
@@ -106,30 +110,30 @@ with col2:
                 )
     st.pyplot(corr_fig)
 
-
-
+# -------- model training ------------------
 st.header('Model Training', divider='orange')
 
+# ------- feature selection --------------
+
 if 'features_included' not in st.session_state:
-    st.session_state['features_included'] = df.columns[:-1].tolist()
+    st.session_state['features_included'] = df.drop(st.session_state['target'], axis=1).columns.tolist()
 if 'features_excluded' not in st.session_state:
-    st.session_state['features_excluded'] = df.columns[:-1].tolist()
+    st.session_state['features_excluded'] = None
 
-
-with st.expander('Model Selection'):
+with st.expander('Feature Selection'):
     include_options = st.multiselect(
     'Features included in the model',
-    df.columns[:-1].tolist(),
-    df.columns[:-1].tolist())
+    df.drop(st.session_state['target'], axis=1).columns.tolist(),
+    df.drop(st.session_state['target'], axis=1).columns.tolist())
 
-    st.write('You selected:', include_options)
-    exclude_options = [x for x in df.columns[:-1].tolist() if x not in include_options]
+    st.write('The model includes : ', include_options)
+    exclude_options = [x for x in df.drop(st.session_state['target'], axis=1).columns.tolist() if x not in include_options]
 
     st.session_state['features_included'] = include_options
     st.session_state['features_excluded'] = exclude_options
 
-X = df.drop(columns=['target']).loc[:,st.session_state['features_included']]
-Y = df['target']
+X = df.drop(columns=[st.session_state['target']]).loc[:,st.session_state['features_included']]
+Y = df[st.session_state['target']]
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=st.session_state['test_size'], random_state=st.session_state['random_state'])
 st.subheader('Data structure')
@@ -139,11 +143,12 @@ with st.container():
         st.write('')
     with col2:
         st.code(f"""
-                    Shape of Predictor dataset  :   {X.shape} \n
-                    Shape of Trainng dataset    :   {x_train.shape} \n
-                    Shape of Testing dataset    :   {x_test.shape} \n
-                    Number of Class             :   {len(np.unique(Y))} \n
-                    Target Class                :   {(*target_class_name,)} \n
+                    Shape of predictor dataset  :   {X.shape} \n
+                    Shape of trainng dataset    :   {x_train.shape} \n
+                    Shape of testing dataset    :   {x_test.shape} \n
+                    Number of class             :   {len(np.unique(Y))} \n
+                    Target column name          :   {st.session_state['target']}\n
+                    Target class                :   {(*target_class_name,)} \n
                     Features included           :   {len(st.session_state['features_included'])} features, {st.session_state['features_included']}\n
                     Features excluded           :   {len(st.session_state['features_excluded'])} features, {st.session_state['features_excluded']}\n
                     Random_state                :   {st.session_state['random_state']}
@@ -174,6 +179,10 @@ if model:
     with col3:
         st.write('')
 
+else:
+    pass
+
+# ----------- model prediction --------------------
 st.header('Model Prediction', divider='orange')
 
 y_test_preds = model.predict(x_test)
@@ -210,7 +219,7 @@ with col3:
         st.write('')
         st.write(f'''{st.session_state['algorithm']} don't offer a direct feature importance calculation''')
 
-
+# --------- model evaluation ----------------
 st.header('Model Evaluation', divider='orange')
 
 # Overall metrics
@@ -237,7 +246,7 @@ with st.expander('Details of classification report'):
     with st.container():
         col1, col2 = st.columns([2,1])
         with col1:
-            st.markdown(f'''
+            st.markdown('''
                     - <b>Accuracy</b> - It represents the ratio of correctly predicted labels to the total predicted labels\n
                         &emsp;&emsp;&emsp;&emsp;$$Accuracy = (TP + TN) / (TP + FP + TN + FN)$$
                     - <b>Precision (Positive predictive rate)</b>- It represents the ratio of number of actual positive class correctly predicted to
